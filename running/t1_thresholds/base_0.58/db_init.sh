8417348133543b52a104f127e955e25ba2dcaf52
zhinuanguo <zhinuanguo@softwareimprovementgroup.com>
2025-05-21 16:24:55 +0200

    add waiting time for embedding model

--- running/t1_thresholds/base_0.58/db_init.sh
+++ running/t1_thresholds/base_0.58/db_init.sh
@@ -60,7 +60,7 @@ kubectl create secret docker-registry "awssecret" \
   --docker-username=AWS \
   --docker-password="$DOCKER_PAS"
 
-sleep 3
+sleep 30
 
 helm install -f /home/otto/thesis/projects/thesis_intern/deployment/data_importer/k8s/values.yaml importer /home/otto/thesis/projects/thesis_intern/deployment/data_importer/k8s
 

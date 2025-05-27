ee8ddd64fc2a60801c0590284e92d4ed541bb67f
zhinuanguo <zhinuanguo@softwareimprovementgroup.com>
2025-05-27 11:04:19 +0200

    add running deepseek distill llama

---
+++ running/t5_deepseek/deepseek_distill_llama/uninstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Uninstall Helm releases
+helm uninstall -n gpu-operator $(helm list -n gpu-operator -q)
+helm uninstall prometheus -n monitoring
+helm uninstall kepler -n kepler
+helm uninstall pgvector
+helm uninstall chat-backend
+helm uninstall importer
+
+kubectl delete -f ~/thesis/projects/thesis_intern/deployment/llm/k8s/deepseek_llama/secret.yaml
+kubectl delete -f ~/thesis/projects/thesis_intern/deployment/llm/k8s/deepseek_llama/deployment.yaml
+kubectl delete -f ~/thesis/projects/thesis_intern/deployment/llm/k8s/deepseek_llama/service.yaml
+kubectl delete -f ~/thesis/projects/thesis_intern/deployment/llm/k8s/deepseek_llama/pvc.yaml
+
+# Delete Kubernetes resources for Embedding
+kubectl delete -f ~/thesis/projects/thesis_intern/deployment/embedding/k8s/e5_large_v2/deployment.yaml
+kubectl delete -f ~/thesis/projects/thesis_intern/deployment/embedding/k8s/e5_large_v2/service.yaml
+kubectl delete -f ~/thesis/projects/thesis_intern/deployment/embedding/k8s/e5_large_v2/pvc.yaml
+
+kubectl delete secret awssecret

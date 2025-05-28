1f1e8414d172ef41aba80a476d40c0563eec1a50
zhinuanguo <zhinuanguo@softwareimprovementgroup.com>
2025-05-28 20:24:09 +0200

    namespace default fix

--- running/t4_indexing/hnsw/install.sh
+++ running/t4_indexing/hnsw/install.sh
@@ -33,7 +33,7 @@ LLM_POD_NAME=$(kubectl get pods -n "$NAMESPACE" --no-headers -o custom-columns="
 
 echo "Waiting for pod $LLM_POD_NAME to be in Ready status..."
 
-kubectl wait --for=condition=Ready pod/$LLM_POD_NAME --timeout=120s
+kubectl wait --namespace "$NAMESPACE" --for=condition=Ready pod/$LLM_POD_NAME --timeout=120s
 
 echo "pod $LLM_POD_NAME in Ready status..."
 

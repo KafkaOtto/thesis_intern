c6827b81895acac5a9f218bce361c18e5a15b0ba
zhinuanguo <zhinuanguo@softwareimprovementgroup.com>
2025-06-27 18:37:52 +0200

    increase maintenance_work_mem and pgvector memory

--- deployment/postgre/scripts/hnsw.sql
+++ deployment/postgre/scripts/hnsw.sql
@@ -1,7 +1,7 @@
 \c ragdb ragdb_mgmt_user;
 
 -- Create HNSW index
-SET maintenance_work_mem = '12GB';
+SET maintenance_work_mem = '5GB';
 CREATE INDEX ON rag.text_segments USING hnsw (embedding vector_cosine_ops);
 
 
